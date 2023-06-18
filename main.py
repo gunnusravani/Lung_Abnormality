ab6b4c8f12393f437413c1c95f13fdcb95293470
Gautami Mamidala <gautamisaisatya@gmail.com>
2023-06-18 17:01:24 +0000

    Connected to BQ

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from typing import Annotated
+from datetime import date, datetime
 
 import tensorflow as tf
 from tensorflow import keras
@@ -16,8 +17,35 @@ import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
 
+import json
+from google.cloud import bigquery, storage
+from google.oauth2 import service_account
+
+from fastapi.responses import HTMLResponse
+import pandas as pd
+
+key_path = "cloudkarya-internship-1c013aa63f5f.json"
+bigquery_client = bigquery.Client.from_service_account_json(key_path)
+storage_client = storage.Client.from_service_account_json(key_path)
+
+project_id = "cloudkarya-internship"
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class ImageData(BaseModel):
+    img_file: str
+    img_type: str
+    patient_id: str
+    patient_name: str
+    patient_dob: date
+    patient_gender: str
+    patient_email: str
+    pneumonia_prob: float
+    tuberculosis_prob: float
+    cancer_prob: float
+    covid19_prob: float
+
 
 templates = Jinja2Templates(directory="templates")
 
@@ -36,11 +64,13 @@ async def report_file(request: Request,image:Annotated[UploadFile, File(...)],
                        Gender: Annotated[str,Form(...)],
                        image_Type:Annotated[str,Form(...)]
                        ):
+
     # Process the file path
     # Your logic here
     # form = await request.form()
     # file_field = form["image"]
     # file_path = file_request.file_path
+    
     contents = await image.read()
     img = Image.open(io.BytesIO(contents))
     img = img.resize((150, 150))
@@ -67,7 +97,57 @@ async def report_file(request: Request,image:Annotated[UploadFile, File(...)],
         "prediction": predictions1[0][0]
     }
 
+    image_path = f'"https://storage.cloud.google.com/lung_abn/Lung_Images/{image}'
+    #dob = datetime.strptime(item.patient_dob, '%Y-%m-%d').date()
+    dob = datetime.now()
+
+    query = f"""
+    INSERT INTO `{project_id}.ImageData.ImageDataTable`
+    VALUES ('{image_path}', '{image_Type}', 'Need_to_send_var_for_patient_id', '{patient_Name}', 
+            DATE('{dob}'), '{Gender}', '{patient_Email}', 
+            {pred1}, {pred2}, {pred3}, {0})
+    """
+    job = bigquery_client.query(query)
+    job.result()
+    
+
     return templates.TemplateResponse("base.html", {"request": request,  "result1":pred1,"result2":pred2,"result2":pred3, "img":image, "patient_Name":patient_Name,"patient_Age":patient_Age,"patient_Email":patient_Email,"Gender":Gender,"Uploaded_image":image_Type})
+
+
+@app.post("/ImageData/")
+async def create_image_data(item: ImageData):
+    query = f"""
+    INSERT INTO `{project_id}.ImageData.ImageDataTable`
+    VALUES ('{item.img_file}', '{item.img_type}', '{item.patient_id}', '{item.patient_name}', 
+            DATE('{item.patient_dob}'), '{item.patient_gender}', '{item.patient_email}', 
+            {item.pneumonia_prob}, {item.tuberculosis_prob}, {item.cancer_prob}, {item.covid19_prob})
+    """
+    job = bigquery_client.query(query)
+    job.result()  # Wait for the query to complete
+
+    return {"message": "Data inserted successfully"}
+
+
+@app.get("/ImageDatas",response_class=HTMLResponse)
+async def get_image_data():
+   query = f"""
+         SELECT  * FROM {project_id}.ImageData.ImageDataTable;
+   """
+   df = bigquery_client.query(query).to_dataframe()
+   # df.head()
+   return df.to_html()
+
+
+@app.get("/ImageData/{id}",response_class=HTMLResponse)
+async def get_image_data(id):
+   query = f"""
+         SELECT  * FROM {project_id}.ImageData.ImageDataTable
+         WHERE patient_id = '{id}';
+   """
+   df = bigquery_client.query(query).to_dataframe()
+   # df.head()
+   return df.to_html()
+
 
 # # if __name__ == '__dynamic__':
 # #    uvicorn.run(app, host='0.0.0.0', port=8000)
